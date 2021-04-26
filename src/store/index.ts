1e6c265e4ac3192823804f4e0ce4e838fc1f0209
Bogdan Vasile <vasileeduardbogdan@gmail.com>
2021-04-26 15:12:59 +0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,29 +7,11 @@ export default createStore({
 		currentUser: {}
 	},
 	mutations: {
-		async SET_ALL_MESSAGES(state) {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			await import("../mockapi.js")
-				.then((api) => {
-					// Do something with the module.
-					state.conversation = JSON.parse(api.conversation)
-				})
-				.catch((error) => {
-					console.log(error)
-				})
+		SET_ALL_MESSAGES(state, payload) {
+			state.conversation = payload
 		},
-		async SET_CURRENT_USER(state) {
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			await import("../mockapi.js")
-				.then((api) => {
-					// Do something with the module.
-					state.currentUser = JSON.parse(api.currentUser)
-				})
-				.catch((error) => {
-					console.log(error)
-				})
+		SET_CURRENT_USER(state, payload) {
+			state.currentUser = payload
 		},
 		addNewMessage(state, payload) {
 			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -38,11 +20,28 @@ export default createStore({
 		}
 	},
 	actions: {
-		setAllMessages(context) {
-			context.commit("SET_ALL_MESSAGES")
+		async setAllMessages(context) {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			await import("../mockapi.js")
+				.then((api) => {
+					// Do something with the module.
+					context.commit("SET_ALL_MESSAGES", JSON.parse(api.conversation))
+				})
+				.catch((error) => {
+					console.log(error)
+				})
 		},
-		setCurrentUser(context) {
-			context.commit("SET_CURRENT_USER")
+		async setCurrentUser(context) {
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			await import("../mockapi.js")
+				.then((api) => {
+					context.commit("SET_CURRENT_USER", JSON.parse(api.currentUser))
+				})
+				.catch((error) => {
+					console.log(error)
+				})
 		},
 		addNewMessage(context) {
 			context.commit("addNewMessage")
